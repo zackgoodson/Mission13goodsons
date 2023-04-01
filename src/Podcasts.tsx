f8d3536fd6df372c7bf7f31905995c5878aee562
zackgoodson <zackgoodson@gmail.com>
2023-03-31 21:35:38 -0600

    Mission13

---
+++ src/Podcasts.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Podcasts() {
+  return (
+    <React.Fragment>
+      <h1>Click here for cool podcasts!</h1>
+      <a className="button button-large" href="https://baconsale.com/">
+        Podcasts
+      </a>
+    </React.Fragment>
+  );
+}
+
+export default Podcasts;
